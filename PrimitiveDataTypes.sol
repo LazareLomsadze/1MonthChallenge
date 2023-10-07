b687a04b265ac8b51d711b7a152c155d2c88f22e
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-07 21:15:50 +0400

    3. PrimitiveDataTypes.sol

--- PrimitiveDataTypes.sol
+++ PrimitiveDataTypes.sol
@@ -1,1 +1,23 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract Primitives {
+ bool public boo = true;
+ 
+ uint8 public u8 = 1;
+ uint public u256 = 456;
+ uint public u = 123;
+ 
+ int8 public i8 = -1;
+ int public i256 = 456;
+ int public i = -123;
+ 
+ address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+ 
+ bytes1 a = 0xb5;
+ bytes1 b = 0x56;
+ 
+ bool public defaultBoo;
+ uint public defaultUint;
+ int public defaultInt;
+ address public defaultAddr;
+}
