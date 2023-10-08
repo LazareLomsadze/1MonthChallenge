a549ab9bac83e6aa965722ef96b152a5bea24b7c
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-08 12:31:45 +0400

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract Variables {
+ string public text = "Hello";
+ uint
+public num = 123;
+function doSomething() public {
+ uint i = 456;
+ uint timestamp = block.timestamp;
+ address sender = msg.sender;
+ // Rest of the function code…
+ }
+}
+
