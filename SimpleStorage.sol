2316e7f82c3aeea82deb88b05993b2fe4850885f
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-10 11:28:49 +0400

    Day 6

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,1 +1,12 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract SimpleStorage {
+uint public num;
 
+function set(uint _num) public {
+num = _num;
+}
+function get() public viev returns(uint) {
+return num;
+}
+}
