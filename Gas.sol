a2ddfcdffedd555c14e77474b697c85eb267f3d0
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-11 22:28:37 +0400

    Day 7

--- Gas.sol
+++ Gas.sol
@@ -1,1 +1,22 @@
+Calculating Gas Spent:
+
+contract GasExample {
+ function calculateGas() public pure returns (uint) {
+ uint startGas = gasleft();
+ // Perform computations or operations
+ uint endGas = gasleft();
+ return startGas - endGas;
+ }
+}
+
+Setting Gas Price:
+
+pragma solidity ^0.8.0;
+contract GasPriceExample {
+ uint public gasPrice;
+function setGasPrice(uint _gasPrice) public {
+ require(_gasPrice > 0, "Gas price must be greater than zero");
+ gasPrice = _gasPrice;
+ }
+}
 
