30466c83fdd92547f867997775638aebaab1fe8a
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-12 19:48:11 +0400

    Day 8

---
+++ While.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract Loop {
+ function loop() public {
+ // while loop
+ uint j;
+ while (j < 10) {
+ j++;
+ // Code block executed for each iteration
+ }
+ }
+}
