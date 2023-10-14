cdd91d5ab15f4b5000fc49d2ad58a4c4c8dc1be3
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-14 21:29:06 +0400

    create enum.sol

---
+++ Enum.sol
@@ -0,0 +1,22 @@
+contract Enum {
+ enum Status {
+ Pending,
+ Shipped,
+ Accepted,
+ Rejected,
+ Canceled
+ }
+Status public status;
+function get() public view returns (Status) {
+ return status;
+ }
+function set(Status _status) public {
+ status = _status;
+ }
+function cancel() public {
+ status = Status.Canceled;
+ }
+function reset() public {
+ delete status;
+ }
+}
