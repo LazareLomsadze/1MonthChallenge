37f124582a9447fe5697ec408d70bbffb043ee86
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-15 15:10:05 +0400

    Update Storage.sol

--- DataLocations/Storage.sol
+++ DataLocations/Storage.sol
@@ -1,1 +1,15 @@
-storage - variable is a state variable (store on blockchain) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// storage - variable is a state variable (store on blockchain) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+function f() public {
+ _f(arr, map, myStructs[1]);
+MyStruct storage myStruct = myStructs[1];
+ MyStruct memory myMemStruct = MyStruct(0);
+}
+function _f(
+ uint[] storage _arr,
+ mapping(uint => address) storage _map,
+ MyStruct storage _myStruct
+) internal {
+ // Perform operations with storage variables
+}
