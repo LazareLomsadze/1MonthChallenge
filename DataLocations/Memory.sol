fe59d21202493c945b57e207c7d8dc94fec73a6e
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-15 15:10:19 +0400

    Update Memory.sol

--- DataLocations/Memory.sol
+++ DataLocations/Memory.sol
@@ -1,1 +1,6 @@
 // memory - variable is in memory and it exists while a function is being called !!!!!!!!!!!!!!!!!!!!!!!!
+
+
+function g(uint[] memory _arr) public returns (uint[] memory) {
+ // Perform operations with memory array
+}
