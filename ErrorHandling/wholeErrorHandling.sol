d6cb5344887c32ac52e5fff9095cc3140d1944fc
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-16 14:23:16 +0400

    Day 12

---
+++ ErrorHandling/wholeErrorHandling.sol
@@ -0,0 +1,18 @@
+
+
+function deposit(uint _amount) public {
+ uint oldBalance = balance;
+ uint newBalance = balance + _amount;
+require(newBalance >= oldBalance, "Overflow");
+balance = newBalance;
+assert(balance >= oldBalance);
+}
+function withdraw(uint _amount) public {
+ uint oldBalance = balance;
+require(balance >= _amount, "Underflow");
+if (balance < _amount) {
+ revert("Underflow");
+ }
+balance -= _amount;
+assert(balance <= oldBalance);
+}
