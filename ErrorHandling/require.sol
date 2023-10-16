c9374c69f3abbcf6f490bcea729cdbb2933f39ba
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-16 14:16:18 +0400

    Update require.sol

--- ErrorHandling/require.sol
+++ ErrorHandling/require.sol
@@ -1,1 +1,16 @@
+//  The require function in smart contracts is like a security checkpoint. It checks if certain conditions are met before allowing the code to proceed. If the condition isn't met, it stops the execution and reverts any changes made, like hitting an "undo" button. This helps ensure that the contract's state remains safe and consistent. require is used to validate inputs, verify conditions, and ensure proper function outcomes. It's a critical tool for maintaining the integrity of a contract's data and operations.
+
+//  require(_i > 10, “Input must be greater than 10”);
+
+// function withdraw(uint amount) public {
+    require(amount > 0, "Amount must be greater than zero");
+    // Rest of the code for withdrawal
+}
+
+//  function onlyAdmin() public view {
+    require(msg.sender == adminAddress, "Only admin can call this function");
+    // Code for admin-only functionality
+}
+
+
 
