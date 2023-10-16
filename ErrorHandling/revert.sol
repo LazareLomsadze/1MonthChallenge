e0a5875c9c3d4dc43522c568cd4081b7cf408ba5
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-16 14:18:58 +0400

    Update revert.sol

--- ErrorHandling/revert.sol
+++ ErrorHandling/revert.sol
@@ -1,1 +1,25 @@
+// The revert function in smart contracts is like a sophisticated alarm system. It's used to stop and undo any changes if certain conditions aren't met, just like require. But what makes it special is that it allows for more detailed and customized error messages. So, it's perfect for situations where you need to explain in detail why a transaction couldn't proceed. Think of it as a way to provide clear feedback to users about what went wrong. This helps make smart contracts more user-friendly and transparent.
+
+function withdraw(uint amount) public {
+    if (amount == 0) {
+        revert("Withdrawal amount cannot be zero");
+    }
+    // Rest of the code for withdrawal
+}
+
+
+function onlyAdmin() public view {
+    if (msg.sender != adminAddress) {
+        revert("Only admin can call this function");
+    }
+    // Code for admin-only functionality
+}
+
+
+function add(uint a, uint b) public pure returns (uint) {
+    if (a + b < a) {
+        revert("Sum would result in overflow");
+    }
+    return a + b;
+}
 
