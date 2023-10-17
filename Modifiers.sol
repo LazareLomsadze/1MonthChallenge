85725140df0afa648d6de33a4bf41f99be32c3b8
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-17 12:03:35 +0400

    Day 13

--- Modifiers.sol
+++ Modifiers.sol
@@ -1,1 +1,17 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract FunctionModifier {
+ address public owner;
+ uint public x = 10;
+ bool public locked;
+constructor() {
+ owner = msg.sender;
+ }
+modifier onlyOwner() {
+ require(msg.sender == owner, "Not owner");
+ _;
+ }
+function changeOwner(address _newOwner) public onlyOwner {
+owner = _newOwner;
+}
+}
