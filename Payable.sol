f2ec0654dc519612474a984b3a5e0461c710ea1f
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-21 20:28:16 +0400

    Day 15

--- Payable.sol
+++ Payable.sol
@@ -1,3 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+contract AnotherPayable {
+ address public owner;
+constructor() {
+ owner = msg.sender;
+ }
+function deposit() public payable {}
+function notPayable() public {}
+function withdraw() public {
+ require(msg.sender == owner, "Only owner can withdraw");
+ uint amount = address(this).balance;
+ payable(msg.sender).transfer(amount);
+ }
+function transfer(address _to, uint _amount) public {
+ require(msg.sender == owner, "Only owner can transfer");
+ payable(_to).transfer(_amount);
+ }
+}
