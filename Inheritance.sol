436874b82d274ca7dd20b70a1a5d77c46fdd8eeb
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-21 19:59:09 +0400

    Update Inheritance.sol

--- Inheritance.sol
+++ Inheritance.sol
@@ -1,1 +1,51 @@
+/* Graph of inheritance
+ A
+ / \
+ B C
+ / \ /
+F D,E
+*/
+contract A {
+ function foo() public pure virtual returns (string memory) {
+ return "A";
+ }
+}
+contract B is A {
+ function foo() public pure virtual override returns (string memory) {
+ return "B";
+ }
+}
+contract C is A {
+ function foo() public pure virtual override returns (string memory) {
+ return "C";
+ }
+}
+contract D is B, C {
+ function foo() public pure override(B, C) returns (string memory) {
+ return super.foo();
+ }
+}
+contract E is C, B {
+ function foo() public pure override(C, B) returns (string memory) {
+ return super.foo();
+ }
+}
+contract F is A, B {
+ function foo() public pure override(A, B) returns (string memory) {
+ return super.foo();
+ }
+}
 
+
+1)  Contract A is the base contract with a function foo(). This function is marked as virtual, indicating that it can be overridden by child contracts. The default implementation returns the string "A".
+2)  Contract B inherits from A using the is keyword. This means that B is a child contract of A and inherits the foo() function.
+B overrides the foo() function using the override keyword. This means that B provides its own implementation of foo(), which will be used instead of the one defined in A. In this case, it returns the string "B".
+3)  Contract C also inherits from A. This makes C a sibling contract of B, both inheriting from A.
+C also overrides the foo() function, returning the string "C".
+4)  Contract D inherits from both B and C, so it has access to the foo() functions from both B and C.
+D overrides foo(), and it explicitly specifies which parent contract's implementation it wants to use (in this case, B and C). It then calls super.foo() to invoke the overridden function from one of its parent contracts.
+
+finally)  Contract F inherits from both A and B.
+It overrides foo() and specifies that it wants to use the implementations from both A and B.
+It then calls super.foo() to invoke the overridden function from one of its parent contracts.
+Overall, this inheritance structure allows for a complex relationship between the contracts, and it demonstrates how function overriding works in Solidity. Child contracts can provide their own implementations of functions inherited from parent contracts, allowing for customization and flexibility in the behavior of smart contracts.
