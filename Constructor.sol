3c423bd1d79fc3081c17576bae8735497517f63f
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-21 18:25:33 +0400

    Day 15

--- Constructor.sol
+++ Constructor.sol
@@ -1,1 +1,26 @@
+// Parent contract X
+contract X {
+    string public inputX;
+    
+    constructor(string memory _inputX) {
+        inputX = _inputX;
+    }
+}
 
+// Parent contract Y
+contract Y {
+    string public inputY;
+    
+    constructor(string memory _inputY) {
+        inputY = _inputY;
+    }
+}
+
+// Child contract B inheriting from X and Y
+contract B is X("Input to X"), Y("Input to Y") {
+    string public combinedInput;
+    
+    constructor() {
+        combinedInput = string(abi.encodePacked(inputX, " ", inputY));
+    }
+}
