ffdbdac841da53e8d17b883c15ffd68de9daf146
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-22 19:36:12 +0400

    Update transfer.sol

--- transfer.sol
+++ transfer.sol
@@ -1,1 +1,7 @@
+//  The `transfer` method serves as the simplest way to send Ether. It transfers a fixed amount of gas (2300 gas) and automatically throws an error if the transfer fails. Here’s an example of using the `transfer` method in Solidity:
+
+function sendEtherUsingTransfer(address payable _to) public payable {
+_to.transfer(msg.value);
+}
+
 
