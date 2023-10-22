c92651e8abb1c0b8f2963e20e7cf8d931a40a2b1
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-22 19:40:15 +0400

    Update call.sol

--- call.sol
+++ call.sol
@@ -1,1 +1,16 @@
+// The `call` method stands as the most versatile way to send Ether and interact with other contracts.
+It allows for forwarding all available gas or specifying a gas limit, 
+and provides a boolean return value indicating the success or failure of the transfer. Here’s an example of using the `call` method:
+
+
+``solidity
+function sendEtherUsingCall(address payable _to) public payable {
+(bool sent, bytes memory data) = _to.call{value: msg.value}(“”);
+require(sent, “Failed to send Ether”);
+}
+```
+
+//  After December 2019, the `call` method became the recommended approach for sending Ether. 
+It grants more control over gas usage and facilitates complex interactions between contracts.
+However, it’s crucial to implement appropriate security measures to guard against re-entrancy attacks.
 
