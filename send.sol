ed8a259511ed3544b42361f30e9b67ea928b7edd
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-22 19:38:57 +0400

    Update send.sol

--- send.sol
+++ send.sol
@@ -1,1 +1,12 @@
+//  The `send` method closely resembles `transfer`, but it provides a boolean return value to indicate the success or failure of the transfer. Similar to `transfer`, it also sends a fixed amount of gas (2300 gas) and reverts if the transfer fails. Here’s an example of using the `send` method:
+
+```solidity
+function sendEtherUsingSend(address payable _to) public payable {
+bool sent = _to.send(msg.value);
+require(sent, “Failed to send Ether”);
+}
+```
+
+//  Despite the `send` method providing a return value, it is no longer recommended for sending Ether due to potential re-entrancy attacks and the limited gas provided. It is advisable to use the `call` method instead.
+
 
