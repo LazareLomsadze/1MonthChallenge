b391ac59b7ddbf39dfdef582d6e278fed674c778
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-24 13:15:01 +0400

    Day18

--- Call.sol
+++ Call.sol
@@ -1,1 +1,31 @@
+//  It's A call documentation but it's not recomended
+//  Better way is send, not call.
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract Receiver {
+ event Received(address caller, uint amount, string message);
+fallback() external payable {
+ emit Received(msg.sender, msg.value, "Fallback was called");
+ }
+function foo(string memory _message, uint _x) public payable returns (uint) {
+ emit Received(msg.sender, msg.value, _message);
+return _x + 1;
+ }
+}
+contract Caller {
+ event Response(bool success, bytes data);
+function testCallFoo(address payable _addr) public payable {
+ (bool success, bytes memory data) = _addr.call{value: msg.value, gas: 5000}(
+ abi.encodeWithSignature("foo(string,uint256)", "call foo", 123)
+ );
+emit Response(success, data);
+ }
+function testCallDoesNotExist(address payable _addr) public payable {
+ (bool success, bytes memory data) = _addr.call{value: msg.value
+}(
+ abi.encodeWithSignature("doesNotExist()")
+ );
+emit Response(success, data);
+ }
+}
