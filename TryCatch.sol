c14da127a932cc09bbaabfd6ca7bb9f0a719034a
itachi2003 <lomsadze.lazare2003@gmail.com>
2023-11-02 21:23:26 +0400

    add code sol

--- TryCatch.sol
+++ TryCatch.sol
@@ -3,3 +3,11 @@
 // Handling Errors in External Function Calls ⚙️
 
 // One of the primary use cases of try-catch in Solidity is handling errors that may arise during external function calls. 📞 In the provided code sample, we have two contracts, `Foo` and `Bar`. The `Bar` contract utilizes an instance of the `Foo` contract to demonstrate the try-catch mechanism with external function calls. Let’s analyze the code snippet and its output. 🔄
+
+function tryCatchExternalCall(uint _i) public {
+ try foo.myFunc(_i) returns (string memory result) {
+ emit Log(result);
+ } catch {
+ emit Log("external call failed");
+ }
+}
