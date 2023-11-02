85b56fc03ce84df2024bf3ee60f4b3ed85ab3544
“itachi2003” <lomsadze.lazare2003@gmail.com>
2023-11-02 21:11:29 +0400

    add comment but test something

--- TryCatch.sol
+++ TryCatch.sol
@@ -1,3 +1,5 @@
 // Solidity, the programming language for Ethereum smart contracts, offers a powerful error handling mechanism known as try-catch.
 
 // Handling Errors in External Function Calls ⚙️
+
+// One of the primary use cases of try-catch in Solidity is handling errors that may arise during external function calls. 📞 In the provided code sample, we have two contracts, `Foo` and `Bar`. The `Bar` contract utilizes an instance of the `Foo` contract to demonstrate the try-catch mechanism with external function calls. Let’s analyze the code snippet and its output. 🔄
