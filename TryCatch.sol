095162d4cba5d4b82bceabb964f8de1300ee4461
“itachi2003” <lomsadze.lazare2003@gmail.com>
2023-11-02 21:07:02 +0400

    add comment

--- TryCatch.sol
+++ TryCatch.sol
@@ -1,1 +1,3 @@
 // Solidity, the programming language for Ethereum smart contracts, offers a powerful error handling mechanism known as try-catch.
+
+// Handling Errors in External Function Calls ⚙️
