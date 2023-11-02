31dc736b2a35ac229db662d408a252cb6be03af6
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-11-02 20:26:39 +0400

    Day 20

--- Calling-Other-Contract.sol
+++ Calling-Other-Contract.sol
@@ -1,1 +1,10 @@
-
+pragma solidity ^0.8.17;
+contract Caller {
+ function setXFromAddress(address _addr, uint _x) public {
+ Callee callee = Callee(_addr);
+ callee.setX(_x);
+ }
+function setXandSendEther(Callee _callee, uint _x) public payable {
+ (uint x, uint value) = _callee.setXandSendEther{value: msg.value}(_x);
+ }
+}
