c7e2690fa88b5b5af8565f24010e4f84e93d4cee
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-11-08 22:14:18 +0400

    Update BitwiseOperators.sol

--- BitwiseOperators.sol
+++ BitwiseOperators.sol
@@ -1,1 +1,9 @@
+// Solidity supports the following bitwise operators:
+
+// - Bitwise AND (`&`) ✖️
+// - Bitwise OR (`|`) ➕
+// - Bitwise XOR (`^`) ➖
+// - Bitwise NOT (`~`) ⌛
+// - Bitwise Left Shift (`<<`) ⬅️
+// - Bitwise Right Shift (`>>`) ➡️
 
