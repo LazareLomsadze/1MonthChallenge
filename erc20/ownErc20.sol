c1092c304ac28bfca013509bed1fcfbf93b0826b
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-11-15 23:47:01 +0400

    Day 28

--- erc20/ownErc20.sol
+++ erc20/ownErc20.sol
@@ -1,1 +1,9 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+contract MyToken is ERC20 {
+ constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+ // Mint 100 tokens to the contract deployer (msg.sender)
+ _mint(msg.sender, 100 * 10 ** uint(decimals()));
+ }
+}
