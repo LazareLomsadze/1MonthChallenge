e28a7d64494c569be4094c474ada48476cb0c710
BlockHero <lomsadze.lazare2003@gmail.com>
2023-11-17 22:54:45 +0400

    erc721's main function

---
+++ ERC721/erc721MainFunc.sol
@@ -0,0 +1,7 @@
+1. 📈 `balanceOf`: This mystical function reveals the number of tokens owned by a specific address.
+2. 👑 `ownerOf`: Behold! This function unveils the rightful owner of a token, given its unique token ID.
+3. 🚚 `safeTransferFrom`: Witness the secure transfer of tokens from one address to another, performed with utmost care and diligence.
+4. 🏛️ `approve`: Through this ancient ritual, approval is granted to a trusted address to transfer a specific token on behalf of the owner.
+5. 📜 `getApproved`: Enter the realm of approvals, where the address permitted to manage a particular token shall be revealed.
+6. ✍️ `setApprovalForAll`: A potent incantation that grants or revokes approval for an entire set of tokens.
+7. 🧙‍♂️ `isApprovedForAll`: A mystical gaze into the unknown, revealing whether an address wields approval to manage all tokens of an owner.
